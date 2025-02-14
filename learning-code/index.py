4709322fc056a1339c44d2ce6b849b0db112cbdb
bernat alcudia <bernatalcudia@gmail.com>
2025-02-14 10:46:48 +0100

    added Python Casting

--- learning-code/index.py
+++ learning-code/index.py
@@ -88,3 +88,16 @@ x = bool(5)
 x = bytes(5)	
 x = bytearray(5)	
 x = memoryview(bytes(5))	
+# Python Casting
+x = int(1)   # x = 1
+y = int(2.8) # y = 2
+z = int("3") # = 3
+
+x = float(1)     # x = 1.0
+y = float(2.8)   # y = 2.8
+z = float("3")   # z = 3.0
+w = float("4.2") # w = 4.2
+
+x = str("s1") # x = 's1'
+y = str(2)    # y = '2'
+z = str(3.0)  # z = '3.0'
