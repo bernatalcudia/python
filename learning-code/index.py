e48c6c7ffc7ffecca32dcb5b7c6e1a9d6d22c47f
bernat alcudia <bernatalcudia@gmail.com>
2025-02-14 09:30:23 +0100

    added more output variables

--- learning-code/index.py
+++ learning-code/index.py
@@ -5,3 +5,17 @@ x,y,z = "Orange","Banana","Cherry"
 x = y = z = "Orange"
 # Unpack a Collection
 fruits = ["apple","banana","cherry"]
+# Output Variables
+x = "Python is awesome"
+print(x)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+x=5
+y= 10
+print(x + y)
