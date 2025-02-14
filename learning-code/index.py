ada206d00ede0bba987fe00171e3f1aaf09d92a8
bernat alcudia <bernatalcudia@gmail.com>
2025-02-14 10:07:46 +0100

    added Setting the Data Type

--- learning-code/index.py
+++ learning-code/index.py
@@ -42,3 +42,34 @@ print("Python is " + x)
 # Getting the Data Type
 x = 5
 print(type(x))
+# Setting the Data Type
+# string
+x = "Hello World
+# int
+x=20
+# float
+x = 20.5
+# complex
+x = 1j	
+# list
+x = ["apple","banana","cherry"]
+# tuple
+x =("apple","banana","cherry")
+# range
+x = range(6)
+# dict
+x = {"name":"John","age":36}
+# set
+x = {"apple","banana","cherry"}
+# frozenset
+x = frozenset({"apple", "banana", "cherry"})	
+# bool
+x = True
+#bytes
+x = b"Hello"
+# bytearray
+x = bytearray(5)	
+# memoryview
+x = memoryview(bytes(5))	
+# x = None	
+x = None	
